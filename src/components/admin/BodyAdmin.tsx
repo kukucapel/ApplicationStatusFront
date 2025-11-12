1f6cf0675f2fd40cec00deff01724df8443b87c8
kukucapel <loopnow0@gmail.com>
2025-11-12 09:40:02 +0300

    Убрать то что недоделано из админки

--- src/components/admin/BodyAdmin.tsx
+++ src/components/admin/BodyAdmin.tsx
@@ -6,13 +6,14 @@ import TableAdmin from './TableAdmin';
 
 const MENU: [string, string][] = [
     ['Соотрудники', 'employees'],
-    ['Роли', 'roles'],
-    ['Структура', 'units/?as=tree'],
+    // ['Роли', 'roles'],
+    // ['Структура', 'units/?as=tree'],
     // ['Оценки', 'raitings'],
 ];
 
 export default function BodyAdmin() {
     const [page, setPage] = useState<number>(0);
+
     return (
         <div className="max-w-7xl mt-10 mx-auto px-4 sm:px-6 lg:px-8 pb-8">
             <div className="flex flex-col lg:flex-row gap-6">
