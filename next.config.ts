394a13055db07b2bf7be12bcd3ac56c6bf897270
kukucapel <loopnow0@gmail.com>
2025-12-03 08:42:51 +0300

    Возвращение

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/v1/:path*',
-        destination: 'http://192.168.8.12:5000/api/v1/:path*',
-      },
-    ];
-  },
+  basePath: '/status',
 };
 
 export default nextConfig;
