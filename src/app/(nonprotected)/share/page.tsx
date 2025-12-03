18299c6e6857c9fb106378e3a8ef55be21c542c3
kukucapel <loopnow0@gmail.com>
2025-12-03 10:09:31 +0300

    Хотфикс

--- src/app/(nonprotected)/share/page.tsx
+++ src/app/(nonprotected)/share/page.tsx
@@ -2,13 +2,14 @@
 
 import HeaderShare from '@/components/share/HeaderShare';
 import ShareClient from '@/components/share/ShareClient';
+import { Suspense } from 'react';
 export default function SharePage() {
     return (
-        <>
+        <Suspense fallback={<div>Загрузка...</div>}>
             <HeaderShare />
             <main className="max-w-[56rem] ml-auto mr-auto p-10">
                 <ShareClient />
             </main>
-        </>
+        </Suspense>
     );
 }
