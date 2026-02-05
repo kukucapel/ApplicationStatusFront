fcaf13ad7e8820ac101835a8f5bc1b2f330773d1
kukucapel <loopnow0@gmail.com>
2026-02-05 15:18:09 +0300

    Files

--- src/lib/download.ts
+++ src/lib/download.ts
@@ -6,25 +6,5 @@ export default async function downloadFile(
 ) {
   const res = await getUrlDownloadAttachmentLink(idAttachment, token);
   const fileUrl = res.url;
-
-  try {
-    const response = await fetch(fileUrl);
-    const blob = await response.blob();
-
-    const blobUrl = URL.createObjectURL(blob);
-    const newTab = window.open(blobUrl, '_blank');
-
-    if (!newTab) return;
-
-    const a = newTab.document.createElement('a');
-    a.href = blobUrl;
-    a.download = 'file';
-    newTab.document.body.appendChild(a);
-    a.click();
-
-    setTimeout(() => {
-      newTab.close();
-      URL.revokeObjectURL(blobUrl);
-    }, 10);
-  } catch {}
+  window.open(fileUrl, '_blank');
 }
