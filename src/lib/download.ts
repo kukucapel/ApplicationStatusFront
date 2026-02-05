45925d843f29eda545f429e33031402017f496fb
kukucapel <loopnow0@gmail.com>
2026-02-05 12:38:21 +0300

    Files

--- src/lib/download.ts
+++ src/lib/download.ts
@@ -2,24 +2,5 @@ import { getUrlDownloadAttachmentLink } from './updateApplication';
 
 export default async function downloadFile(idAttachment: number) {
   const res = await getUrlDownloadAttachmentLink(idAttachment);
-  const url = res.url;
-
-  const link = document.createElement('a');
-  link.href = url;
-  link.target = '_blank';
-  link.rel = 'noopener noreferrer';
-
-  const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-  if (!isSafari) {
-    link.download = `file_${idAttachment}.${getFileExtension(url)}`;
-  }
-
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-}
-
-function getFileExtension(url: string): string {
-  const match = url.match(/\.([a-zA-Z0-9]+)(?:[?#]|$)/);
-  return match ? match[1] : 'bin';
+  window.open(res.url, '_blank');
 }
